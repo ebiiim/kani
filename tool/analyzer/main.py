b74abe63a07bb249f6477d9bd6b4922a60179cbf
Shunsuke Ise <ise@ebiiim.com>
2021-08-06 03:25:06 +0900

    tool/analyzer read files

--- tool/analyzer/main.py
+++ tool/analyzer/main.py
@@ -1,16 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+from typing import List, Tuple
 import math
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.core.numeric import convolve
 from scipy import signal
+from functools import reduce
 
 
-def _plot(hz, gain, phase=None, title="no title", save=True, ext="png"):
+def _plot(hz, gain, phase=None, title="no title", show=True, save=True, ext="png"):
     fig = plt.figure(figsize=(12, 5))
 
     ax1 = fig.add_subplot(111)
     ax1.set_title(title)
-    ax1.set_ylabel('Gain (dB)')
+    ax1.set_ylabel("Gain (dB)")
     # -----
     # ax1.set_ylim(-18, 18)
     # ax1.set_yticks([-18, -15, -12, -9, -6, -3,
@@ -21,7 +27,7 @@ def _plot(hz, gain, phase=None, title="no title", save=True, ext="png"):
     ax1.set_ylim(-12, 12)
     ax1.set_yticks([-12, -9, -6, -3, 0, 3, 6, 9, 12])
     # -----
-    ax1.set_xlabel('Frequency (Hz)')
+    ax1.set_xlabel("Frequency (Hz)")
     ax1.set_xlim(20, 20000)
     ax1.set_xscale("log")
     ax1.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
@@ -37,7 +43,7 @@ def _plot(hz, gain, phase=None, title="no title", save=True, ext="png"):
 
     if phase is not None:
         ax2 = ax1.twinx()
-        ax2.set_ylabel('Phase (deg.)')
+        ax2.set_ylabel("Phase (deg.)")
         ax2.set_ylim(-180, 180)
         # -----
         # ax2.set_yticks([-180, -150, -120, -90, -60, -30,
@@ -50,32 +56,34 @@ def _plot(hz, gain, phase=None, title="no title", save=True, ext="png"):
 
     # plot
     if phase is not None:
-        ax2.plot(hz, phase, 'cyan')
-    ax1.plot(hz, gain, 'blue')
+        ax2.plot(hz, phase, "cyan")
+    ax1.plot(hz, gain, "blue")
 
     if save:
         plt.savefig(title+"."+ext)
-    plt.show()
+
+    if show:
+        plt.show()
 
 
-def plot_from_coeff(b, a, n, title="no title", fs=48000.0):
+def plot_from_coeff(b, a, n, title="no title", fs=48000.0, show=True, save=True, ext="png"):
     w, h = signal.freqz(b, a, worN=n)
     mag = np.abs(h)
     gain = 20*np.log10(mag/1.0)
     phase = np.angle(h)
     phase_deg = phase*180/math.pi
     hz = fs/2 * w/math.pi  # [0..fs/2]
-    _plot(hz, gain, phase_deg, title+" (Coeff.)")
+    _plot(hz, gain, phase_deg, title, show, save, ext)
 
 
-def plot_from_ir(ir, n, title="no title", fs=48000.0):
+def plot_from_ir(ir, n, title="no title", fs=48000.0, show=True, save=True, ext="png"):
     h = np.fft.fft(ir, n)
     mag = np.abs(h)
     gain = 20*np.log10(mag/1.0)
     phase = np.angle(h)
     phase_deg = phase*180/math.pi
     hz = np.linspace(0, fs, n, endpoint=False)  # [0..fs]
-    _plot(hz, gain, phase_deg, title+" (IR)")
+    _plot(hz, gain, phase_deg, title, show, save, ext)
 
 
 def load_dataset1():
@@ -104,8 +112,165 @@ def load_dataset2():
     return t, b, a, ir
 
 
-# main
-t, b, a, ir = load_dataset1()
-# t, b, a, ir = load_dataset2()
-plot_from_coeff(b, a, n=24000, title=t)
-plot_from_ir(ir, n=len(ir), title=t)
+# returns (result_type, [b], [a], ir)
+#
+# result_type
+#   0: error
+#   1: coefficients
+#   2: impulse response
+#
+# [b]
+#   [[b0, b1, b2, ...], ...]
+#
+# [a]
+#   [[a0, a1, a2, ...], ...]
+#
+# ir
+#   [t0, t1, t2, ...]
+def parse_input(file: str) -> Tuple[int, List[list], List[list], list]:
+    buf = []
+    if file == "":
+        buf = sys.stdin.read().splitlines()
+    else:
+        with open(file) as f:
+            buf = f.read().splitlines()
+    if len(buf) == 0:
+        return 0, [[]], [[]], []
+
+    result_type = 1
+    vb, va = [], []
+    ir = []
+    if buf[0] not in ["b", "a"]:
+        result_type = 2
+
+    # Coeff.
+    if result_type == 1:
+        tmp = []
+        is_a = False
+        for x in buf:
+            if x in ["b", "a"]:
+                if len(tmp) != 0:
+                    if is_a:
+                        va.append(tmp)
+                    else:
+                        vb.append(tmp)
+                    tmp = []
+                if x == "a":
+                    is_a = True
+                else:
+                    is_a = False
+                continue
+            try:
+                tmp.append(float(x))
+            except Exception as e:
+                print(f"{e}", file=sys.stderr)
+                return 0, [[]], [[]], []
+        if is_a:
+            va.append(tmp)
+        else:
+            vb.append(tmp)
+
+    # IR
+    if result_type == 2:
+        try:
+            ir = [float(x) for x in buf]
+        except Exception as e:
+            print(f"{e}", file=sys.stderr)
+            return 0, [[]], [[]], []
+
+    return result_type, vb, va, ir
+
+
+# requires len(v) != 0
+def convolve_coeffs(v: List[list]) -> list:
+    return reduce(lambda x, y: signal.convolve(x, y), v, [1.0])
+
+
+def main():
+    # # demo
+    # t, b, a, ir = load_dataset1()
+    # t, b, a, ir = load_dataset2()
+    # plot_from_coeff(b, a, n=2400, title=t+" (Coeff.)", show=True, save=False)
+    # plot_from_ir(ir, n=2400, title=t+" (IR)", show=True, save=False)
+    # exit(0)
+
+    import argparse
+    from argparse import RawTextHelpFormatter
+    from datetime import datetime as dt
+    import textwrap
+    help = textwrap.dedent("""\
+        Plot frequency response and phase from coefficients or impulse response.
+
+        Coefficients format:
+        ----------
+        b
+        1.234
+        ...
+        a
+        -1.234
+        ...
+        b
+        1.111
+        ...
+        a
+        1.111
+        ...
+        ----------
+
+        Impulse response format:
+        ----------
+        1.111
+        1.234
+        -1.111
+        ...
+        ----------
+        """)
+    parser = argparse.ArgumentParser(
+        description=help, formatter_class=RawTextHelpFormatter)
+    parser.add_argument("-i", dest="file", type=str, nargs=1,
+                        default="", help="input file (default: use stdin)")
+    parser.add_argument("-t", dest="title",  type=str, nargs=1,
+                        default="", help="title (default: {yyyyMMddHHmmss}-{Coeff|IR})")
+    parser.add_argument("-r", dest="rate", type=float, nargs=1,
+                        default=48000.0, help="sampling rate (default: 48000)")
+    parser.add_argument("-n", dest="show",
+                        action="store_false", help="no show plot")
+    parser.add_argument("-s", dest="save",
+                        action="store_true", help="save PNG")
+    # parser.add_argument("--dump", dest="dump", action="store_true", help="dump PNG to stdout")
+
+    args = parser.parse_args()
+    # print(args, file=sys.stderr)
+
+    is_coeff = False
+    file = ""
+    if len(args.file) != 0:
+        file = args.file[0]
+    result_type, vb, va, ir = parse_input(file)
+    if result_type == 0:
+        print("wrong format", file=sys.stderr)
+        exit(1)
+    if result_type == 1:
+        is_coeff = True
+    title = args.title
+    if title == "":
+        title = dt.now().strftime("%Y%m%d%H%M%S")
+        if is_coeff:
+            title += " (Coeff.)"
+        else:
+            title += " (IR)"
+
+    if is_coeff:
+        b = convolve_coeffs(vb)
+        a = convolve_coeffs(va)
+        plot_from_coeff(b, a, n=2400, title=title, fs=args.rate,
+                        show=args.show, save=args.save)
+    else:
+        plot_from_ir(ir, n=2400, title=title, fs=args.rate,
+                     show=args.show, save=args.save)
+
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
